cf07555e2b335d092d9ae6a588a681af15282c5f
DESKTOP-FSFG20J\Kermo <kermoanijarv@gmail.com>
2024-05-30 01:22:35 +0300

    TemplateDrivenForms

--- angular-16-crud/CreditCardAdmin/src/app/creditcards/add/add.component.ts
+++ angular-16-crud/CreditCardAdmin/src/app/creditcards/add/add.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { CreditCard } from 'src/app/models/credit-card';
+import { CreditcardsService } from 'src/app/services/creditcards.service';
 
 @Component({
   selector: 'app-add',
@@ -7,6 +9,11 @@ import { CreditCard } from 'src/app/models/credit-card';
   styleUrls: ['./add.component.scss']
 })
 export class AddComponent {
+
+  constructor(private creditcardsService:CreditcardsService,
+    private router: Router){
+
+  }
 
   newCreditCard: CreditCard = {
     id: undefined,
@@ -25,6 +32,10 @@ export class AddComponent {
 
   saveCreditCard(){
     console.log("Form Submitted");
-    console.log(this.newCreditCard)
+    this.creditcardsService.createCreditCard(this.newCreditCard).subscribe(data => {
+      alert("Credit Card Added");
+      this.router.navigate(['creditcards']);
+    })
+    
   }
 }
